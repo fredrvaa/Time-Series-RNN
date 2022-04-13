48b7f567a52b31eb1dd0e9ed4a67599ac3b9c8c1
fredrvaa <fredrvaa@stud.ntnu.no>
2022-04-13 09:19:05 +0200

    Use TimeseriesGenerator

--- rnn/network.py
+++ rnn/network.py
@@ -4,19 +4,22 @@ import tensorflow.keras.layers as tfkl
 
 class Network:
     def __init__(self,
-                 input_size: int,
-                 output_size: int,
-                 hidden_sizes: list[int],
+                 input_shape: tuple,
+                 output_units: int,
+                 hidden_units: list[int],
                  dropout_rate: float = 1e-1,
                  learning_rate: float = 1e-3,
                  ):
 
-        self.model = tfk.Sequential(
-            tfkl.InputLayer(input_shape=(input_size, )),
-            *[tfkl.LSTM(units=size, dropout=dropout_rate, return_sequences=True) for size in hidden_sizes],
-            tfkl.Dense(units=output_size, dropout=dropout_rate)
+        self.model = tfk.Sequential(layers=[
+                tfkl.InputLayer(input_shape=input_shape),
+                *[tfkl.LSTM(units=units, dropout=dropout_rate, return_sequences=True) for units in hidden_units[:-2]],
+                tfkl.LSTM(units=hidden_units[-2], dropout=dropout_rate),
+                tfkl.Dense(units=hidden_units[-1], activation='relu'),
+                tfkl.Dense(units=output_units, activation=None)
+            ]
         )
 
-        self.model.compile(optimizer=tfk.optimizers.Adam(learning_rate=learning_rate))
+        self.model.compile(optimizer=tfk.optimizers.Adam(learning_rate), loss='mse')
         self.model.summary()
 

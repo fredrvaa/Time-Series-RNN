e2133ac6531e15d1f13d5481bd210641a2ec25d0
fredrvaa <fredrvaa@stud.ntnu.no>
2022-04-13 13:49:42 +0200

    Move fit code to network module

--- rnn/network.py
+++ rnn/network.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
+import pandas as pd
 import tensorflow.keras as tfk
 import tensorflow.keras.layers as tfkl
+from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
 
 
 class Network:
@@ -22,4 +26,23 @@ class Network:
 
         self.model.compile(optimizer=tfk.optimizers.Adam(learning_rate), loss='mse')
         self.model.summary()
+
+    def fit(self,
+            x: pd.DataFrame,
+            y: pd.DataFrame,
+            train_ratio: float = 0.7,
+            epochs: int = 10,
+            sequence_length: int = 144,
+            batch_size: int = 256,
+            callbacks: Optional[list] = None
+            ):
+
+        end_index = int(x.shape[0] * train_ratio)
+        train_gen = TimeseriesGenerator(x, y, length=sequence_length, batch_size=batch_size, end_index=end_index)
+        val_gen = TimeseriesGenerator(x, y, length=sequence_length, batch_size=batch_size, start_index=end_index + 1)
+
+        self.model.fit(train_gen, validation_data=val_gen, epochs=epochs, callbacks=callbacks)
+
+    def multistep_predict(self, ):
+        pass
 
